8ba57738aff340641dcbbc161300ac5293e227b6
Mihir Sarwade <mihir.sarwade@gmail.com>
2014-11-22 15:20:17 -0500

    internal function comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,17 @@
 ## essentially returns a list of (get,set) functions for the matrix and its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
+  ##Setting up get/set fuctions for the matrix
   s <- NULL
   set <- function(y) {
     x <<- y
     s <<- NULL
   }
   get <- function() x
+  ##Setting up get/set functions for inverse of matrix
   setsolve <- function(solve) s <<- solve
   getsolve <- function() s
+  ##Returning list of get/set functions
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
@@ -22,12 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## caches it and returns it. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Checking if inverse of 'x' is cached; if so, display a message and return it
   s <- x$getsolve()
   if(!is.null(s)) {
     message("getting cached data")
     return(s)
   }
+  ## If not, retrieve the matrix, calculate the inverse, set it and return it
   data <- x$get()
   s <- solve(data, ...)
   x$setsolve(s)
